78ed8039e45a8d62aac4b5651938a12ef78bc04b
developer <mailtoibbad@gmail.com>
2016-11-29 23:14:47 +0200

    embed data function working

--- helpers/helper_functions.py
+++ helpers/helper_functions.py
@@ -23,15 +23,6 @@ def generate_secret_key(length=32):
                    for _ in range(length))
 
 
-def get_start_stop_codons():
-    """
-    This function returns a dictionary with list of locations of start and
-    stop codons from
-    :return:
-    """
-    pass
-
-
 def dna_from_mat(filename=None, path=None):
     """
     This function loads the DNA sequence from a .mat file. if both filename
@@ -147,10 +138,10 @@ def find_coding_region(dna_seq=None, frame=1, gc=1):
                 start_index.append(i)
                 start = True
             elif aa == 'stop' and start:
-                stop_index.append(i)
+                stop_index.append(i+3)
                 start = False
         if len(stop_index) < len(start_index):
-            stop_index.append((len(dna)-len(dna) %3) + 1)
+            stop_index.append((len(dna)-len(dna) % 3) + 1)
         return dict(start=start_index, stop=stop_index)
     except KeyError:
         # given GC value does not have any associated file.
@@ -266,9 +257,9 @@ def _clean_dna(dna_seq):
     return ''.join(c for c in dna_seq.lower() if c in 'agct')
 
 
-def str_to_bits(str_input):
+def str_to_bin(str_input):
     """
-    Convert string to bits
+    Convert character string to binary string
     :param str_input: string input
     :return: string containing bits.
     """
@@ -279,9 +270,9 @@ def str_to_bits(str_input):
     return bit_str
 
 
-def bits_to_str(bits):
+def bin_to_str(bits):
     """
-    Convert bits(string) to string
+    Convert binary string to character string.
     :param bits: character array (string) containing bits.
     :return: string
     """
@@ -295,7 +286,7 @@ def str2bits(str_in):
     """
     Convert string input to bits.
     :param str_in: input string
-    :return: bits returned
+    :return: bits array
     """
     result = []
     for c in str_in:
@@ -306,6 +297,11 @@ def str2bits(str_in):
 
 
 def bits2str(bits):
+    """
+    Convert binary array to string.
+    :param bits: bit array.
+    :return: character string.
+    """
     chars = []
     for b in range(int(len(bits) / 8)):
         byte = bits[b * 8:(b + 1) * 8]
@@ -346,6 +342,161 @@ def nucleotide_to_bin(ch):
     elif ch == 'T':
         return '11'
     else:
+        return None
+
+
+def _lsb_4fold(aa, bits):
+    """
+    This function embeds a pair of bits in 4/5/6 fold degenerative codon.
+    :param aa: amino acid information.
+    :param bits: bits (string of length 2 e.g. 00) which should be
+    embedded in codon.
+    :return: watermarked codon (string) e.g. AGA.
+    """
+    codon = _find_popular_codon(aa)
+    if bits == '00':
+        return codon[:2]+'a'
+    elif bits == '01':
+        return codon[:2]+'c'
+    elif bits == '10':
+        return codon[:2]+'g'
+    elif bits == '11':
+        return codon[:2]+'t'
+
+
+def _lsb_2fold(aa, bit):
+    """
+    This function embeds a pair of bits in 2/3 fold degenerative codon.
+    :param aa: amino acid information.
+    :param bit: bit (character 2 e.g. 0) which should be embedded in codon.
+    :return: watermarked codon (string) e.g. AGA.
+    """
+    if bit == '0':
+        print ("2fold sub:", aa["codons"][0], " bit:", bit)
+        return aa["codons"][0]
+    else:
+        print("2fold sub:", aa["codons"][1], " bit:", bit)
+        return aa["codons"][1]
+
+
+def _extract_2fold(codon, aa):
+    """
+    This function embeds a pair of bits in 2/3 fold degenerative codon.
+    :param codon: watermarked codon
+    :param aa: amino acid information.
+    :return: extracted bit 0 or 1.
+    """
+    if codon == aa["codons"][0]:
+        return '0'
+    else:
+        return '1'
+
+
+def _extract_lsb_4fold(codon):
+    """
+    This function returns a pair of bits from LSB of 4 fold synonymous
+    substitution codon.
+    :param codon: codon from which, message needs to be extracted.
+    :return: a pair of binary bits (string format) extracted from LSB of
+    given codon.
+    """
+    if codon[-1] == 'a':
+        return '00'
+    elif codon[-1] == 'c':
+        return '01'
+    elif codon[-1] == 'g':
+        return '10'
+    elif codon[-1] == 't':
+        return '11'
+
+
+def _find_popular_codon(aa):
+    """
+    This function returns popular codon from a 4+ fold degenerative codon.
+    :param aa: dictionary containing amino acid information.
+    :return:
+    """
+    codons = [c[:2] for c in aa["codons"]]
+    counts = []
+    for i in range(len(codons)):
+        pc = codons[i]
+        count = 0
+        for j in range(len(codons)):
+            if codons[j] == pc:
+                count += 1
+        counts.append(count)
+    # find index of the higest entry
+    highest = 0
+    for i in range(len(counts)):
+        if counts[i] > counts[highest]:
+            highest = i
+    return aa["codons"][highest]
+
+
+def embed_data(dna_seq=None, message=None, frame=1, region={}, gc=1):
+    """
+    This function embeds the given message in given DNA sequence.
+    :param dna_seq: DNA sequence (string) to be watermarked.
+    :param message: watermark message (string)
+    :param frame: open reading frame number in which data will be
+    watermarked.
+    :param region: dictionary containing position of start, stop codons
+    for coding regions.
+    :param gc: genetic code.
+    :return: DNA sequence (string) watermarked.
+    """
+    if dna_seq is None or type(dna_seq) is not str:
+        print("here")
+        return None
+    # Clean dna sequence.
+    dna_seq = _clean_dna(dna_seq)
+    wm_dna = ""                             # watermarked DNA
+    # embed data
+    try:
+        # Convert data to binary string
+        wmc = 0                             # watermark counter
+        wm_data = str_to_bin(message)
+        gct = get_gc_table(gc_file_associations.get(str(gc)))
+        dna = dna_seq[
+              (frame-1): (len(dna_seq) - (len(dna_seq) % 3) + (frame - 1))]
+
+        # Finish last coding region at the end of DNA.
+        if len(region.get("start")) < len(region.get("stop")):
+            region.get("stop").append(len(dna))
+        rc = 0
+        i = 0
+        while i < len(dna):
+            # Loop through whole DNA sequence with step size = 3 (i.e. length
+            #  of codon)
+            if rc < len(region.get("start")) and i >= region.get("start")[rc]:
+                # Loop through coding region.
+                j = i
+                while j < region.get("stop")[rc]:
+                    # embed data
+                    aa = get_aa_using_codon_gct(gct=gct, codon=dna[j: j+3])
+                    if aa["count"] > 3 and wmc < len(wm_data):
+                        # embedding in 4+ fold codons
+                        wm_dna += _lsb_4fold(aa=aa, bits=wm_data[wmc:wmc+2])
+                        wmc += 2
+                    elif aa["count"] > 1 and wmc < len(wm_data):
+                        # embedding in 2/3 fold codons
+                        wm_dna += _lsb_2fold(aa=aa, bit=wm_data[wmc])
+                        wmc += 1
+                    else:
+                        wm_dna += dna[j:j+3]
+                    j += 3
+                rc += 1          # update rc for next coding region.
+                i = j            # increment counters accordingly
+            else:
+                # if not in coding region
+                wm_dna += dna[i:i+3]
+                i += 3
+        # append the remaining sequence.
+        if len(wm_dna) < len(dna_seq):
+            wm_dna += dna_seq[len(wm_dna):]
+        return wm_dna.lower()
+    except Exception as e:
+        print(e)
         return None
 
 
---
+++ tests/testing_helpers.py
@@ -0,0 +1,6 @@
+dummy_seq = "aaaatgttattttaacatcacatgtatgcttagaat"
+wm_seq = "aaaatgctcttctaacatcacatgtatgcatagaat"
+         "aaaatgctcttctagcatcacatgtacgcatagaat"
+"""
+nc_start_6f_2f_stop_nc_nc_start_2f_4f_stop_nc
+"""
