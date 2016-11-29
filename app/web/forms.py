908984886584ec4a66d4595689a777a6bd4a36a4
developer <mailtoibbad@gmail.com>
2016-11-30 01:05:57 +0200

    added forms for extract

--- app/web/forms.py
+++ app/web/forms.py
@@ -1,3 +1,28 @@
 """
 This module contains the declarations for the Forms used in web application.
 """
+from flask_wtf import Form
+from wtforms import SubmitField, StringField, IntegerField, SelectField
+from wtforms.validators import number_range, input_required
+
+
+class EmbedForm(Form):
+    """
+    Form for extraction of insertion of watermark in DNA sequence
+    """
+    dna_field = StringField('DNA string', validators=[input_required()])
+    gc_field = IntegerField('Genetic code', validators=[input_required(),
+                                                        number_range(1, 42)])
+    msg_field = StringField('Enter message', validators=[input_required()])
+    submit = SubmitField('Embed')
+
+
+class ExtractForm(Form):
+    """
+    Form for extraction of Data from DNA sequence
+    """
+    dna_field = StringField('Watermarked DNA', validators=[input_required()])
+    gc_field = IntegerField('Genetic code', validators=[input_required(),
+                                                        number_range(1, 42)])
+
+    submit = SubmitField('Extract')
