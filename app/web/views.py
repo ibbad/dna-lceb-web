2c1378ac5ca6b7f08295ac8c8e46ff66721db501
developer <mailtoibbad@gmail.com>
2016-12-01 23:10:18 +0200

    working embed, extract,capacitycalc functions for web

--- app/web/views.py
+++ app/web/views.py
@@ -6,9 +6,8 @@ from .forms import EmbedForm, ExtractForm, CapacityCalculateForm
 from helpers.gc_file_helpers import gc_file_associations
 from flask import flash, redirect, render_template, url_for, abort, request, \
     current_app
-from app.common.helpers import find_coding_region, find_capacity, \
-    find_capacity_for_coding_region, embed_data, extract_data
-
+from app.common.helpers import find_coding_region, find_capacity,\
+    embed_data, extract_data
 
 
 @web.route('/shutdown')
@@ -26,13 +25,13 @@ def server_shutdown():
     return 'Shutting down...'
 
 
-@web.index('/')
+@web.route('/', methods=['GET'])
 def index():
     # TODO: redirect to project index page.
     return redirect(url_for('web.embed'))
 
 
-@web.index('/embed', methods=['GET', 'POST'])
+@web.route('/embed', methods=['GET', 'POST'])
 def embed():
     form = EmbedForm(gc_field=1)
 
@@ -47,7 +46,7 @@ def embed():
                                                 gc=form.gc_field.data)
             cap = find_capacity(dna_seq=seq, frame=1,
                                 gc=form.gc_field.data)
-            if (msg*8) > cap:
+            if (len(msg)*8) > cap:
                 return render_template('errors/400.html',
                                        message='watermark length exceeds '
                                                'capacity.')
@@ -56,12 +55,12 @@ def embed():
             return render_template('result.html',
                                    message='Watermarked DNA:\n'+wm_seq)
         except Exception as e:
-            return render_template('error/400.html', message=str(e))
+            return render_template('errors/400.html', message=str(e))
     return render_template('embed.html', form=form)
 
 
-@web.index('/extract', methods=['GET', 'POST'])
-def embed():
+@web.route('/extract', methods=['GET', 'POST'])
+def extract():
     form = ExtractForm(gc_field=1)
 
     if form.validate_on_submit():
@@ -72,18 +71,18 @@ def embed():
             wm_seq = form.dna_field.data
             coding_regions = find_coding_region(dna_seq=wm_seq, frame=1,
                                                 gc=form.gc_field.data)
-            e_msg = extract_data(dna_seq=wm_seq, frame=1,
+            e_msg = extract_data(wm_dna=wm_seq, frame=1,
                                  region=coding_regions, gc=form.gc_field.data)
             return render_template('result.html',
                                    message='Extracted message:\n'+e_msg)
         except Exception as e:
-            return render_template('error/400.html', message=str(e))
+            return render_template('errors/400.html', message=str(e))
     return render_template('extract.html', form=form)
 
 
-@web.index('/capacity', methods=['GET', 'POST'])
+@web.route('/capacity', methods=['GET', 'POST'])
 def cap_calculate():
-    form = CapacityCalculateForm()
+    form = CapacityCalculateForm(gc_field=1)
     if form.validate_on_submit():
         if str(form.gc_field.data) not in gc_file_associations.keys():
             return render_template('errors/400.html', message='Enter a valid '
@@ -96,20 +95,5 @@ def cap_calculate():
                                            '{bits} bits)'.format(
                                             ltr=int(cap/8), bits=cap))
         except Exception as e:
-            return render_template('error/400.html', message=str(e))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            return render_template('errors/400.html', message=str(e))
+    return render_template('capacitycalc.html', form=form)
