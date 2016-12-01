e0f20250f78b79129897c5f61c27c398abfa10fa
developer <mailtoibbad@gmail.com>
2016-12-02 00:24:33 +0200

    removed logging setup

--- app/api_v1_0/__init__.py
+++ app/api_v1_0/__init__.py
@@ -8,7 +8,7 @@ from app.common.logging import setup_logging
 api = Blueprint('api', __name__)
 
 # Setup logger
-api_log = setup_logging(__name__, 'logs/api.log', maxFilesize=1000000,
-                        backup_count=5)
+# api_log = setup_logging(__name__, 'logs/api.log', maxFilesize=1000000,
+#                         backup_count=5)
 
 from . import views, errors
--- app/web/__init__.py
+++ app/web/__init__.py
@@ -8,7 +8,7 @@ from app.common.logging import setup_logging
 web = Blueprint('web', __name__)
 
 # Setup logger
-web_log = setup_logging(__name__, 'logs/web.log', maxFilesize=1000000,
-                        backup_count=5)
+# web_log = setup_logging(__name__, 'logs/web.log', maxFilesize=1000000,
+#                         backup_count=5)
 
 from . import views, errors
